82ab60de3f685d2c87043eeb005c6832864980ae
Student <student@lab-315-034.cs.csubak.edu>
2018-02-23 15:46:16 -0800

    Changes to my file

--- jorgeZ.cpp
+++ jorgeZ.cpp
@@ -1,2 +1,21 @@
 // Jorge Zuniga
 // My changes
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+
+void screenPrint() {
+    Rect r;
+    int yres = 1024;
+    r.bot = yres - 20;
+    r.left = 10;
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ff0000, "Jesus");
+}
