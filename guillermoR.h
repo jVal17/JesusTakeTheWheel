8af2b57a1069ef8f25c611b35d214e65c50baefd
memoramirez123 <guillermoramirez123@yahoo.com>
2018-02-24 01:18:18 -0800

    Added headers and void prototypes

---
+++ guillermoR.h
@@ -0,0 +1,15 @@
+#ifndef GUILLERMO_R
+#define GUILLERMO_R
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+using namespace std;
+
+void drawBox(); 
+
+#endif
