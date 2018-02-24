c63a0002a0a5d90cef5a5c351c472154fa2daee5
memoramirez123 <guillermoramirez123@yahoo.com>
2018-02-24 01:17:04 -0800

    Added function called 'drawBox()'

--- guillermoR.cpp
+++ guillermoR.cpp
@@ -1,141 +1,30 @@
 //Guillermo Ramirez
 //Seperate file for Guillermo Ramirez
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <math.h>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
-#include <GL/glx.h>
-#include "fonts.h"
+#include "guillermoR.h"
 
-const int NUM_BOXES = 1;
-const int yres = 1024;
-//typedef double Vec[3];
-//static float wid = 120.0f;
-struct Vec {
-	float x, y, z;
-};
-
-struct Shape {
-float width, height;
-int bot, left, cen;
-Vec center;
-int r,g,b;
-};
-
-class Global {
-public:
-    Shape box[NUM_BOXES];
-    Global() {
-	for(int i = 0; i < NUM_BOXES; i++) 
-	{ 
-	    box[i].width = 70; 
-	    box[i].height = 20; 
-	    box[i].center.x = 150+(100*i); 
-	    box[i].center.y = 500-(80*i); 
-	    box[i].r = 100;  
-	    box[i].g = 100;  
-	    box[i].b = 100;  
-	}
-    }
-}gl;
-
-void showBox(){
+void drawBox()
+{
+    
     Rect r;
-    r.bot = yres-20;
-    r.left = 10;
-    r.center = 0;	
-    Shape *s;
-    float w, h;
-    const char* text[NUM_BOXES] = {"IN GUILLERMO FUNCTION"};
-    for(int k = 0; k < NUM_BOXES; k++)
-    {
-	    s = &gl.box[k];
-	    glColor3ub(s->r, s->g, s->b);
-	    glPushMatrix();
-	    glTranslatef(s->center.x, s->center.y, s->center.z);
-	    w = s->width;
-	    h = s->height;
-	    //Adjusts position of the words in comparison to the boxes
-	    r.bot = s->height - 25;
-	    r.left = s->width - 125;
-	    glBegin(GL_QUADS);
-	    glVertex2i(-w, -h);
-	    glVertex2i(-w,  h);
-	    glVertex2i( w,  h);
-	    glVertex2i( w, -h);
-	    glEnd();
-	    ggprint16(&r, 16, 0xCEB121, text[k]);
-	    glPopMatrix();
-    }
+    glColor3f(0.0, 0.0, 0.0);
+    glPushMatrix();
+    glTranslatef(-206.0,512.0,0.0);
+    glBegin(GL_QUADS);
+
+    glVertex2i( 412,  -50);
+    glVertex2i( 412,   50);
+    glVertex2i( 512,   50);
+    glVertex2i( 512,  -50);
+
+    glEnd();
+    glPopMatrix();
+
+    r.bot = 520;
+    r.left = 215;
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ff0000, "Black Box &");
+    ggprint8b(&r, 16, 0x00ff0000, "Text From");
+    ggprint8b(&r, 16, 0x00ff0000, "guillermoR.cpp");
+    glEnd();
 }
 
-
-
-
-
-//class Car {
-//public:
-//        Vec pos;
-//        Vec vel;
-//} car;
-
-//void initCar(int w, int h, GLuint texture, unsigned char *data){
-//int w = img[0].width;
-//int h = img[0].height;
-//Gluint carTexture;
-//img[0].data
-/*glColor3f(1.0, 1.0, 1.0);
-glBindTexture(GL_TEXTURE_2D, texture);
-glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0,
-GL_RGB, GL_UNSIGNED_BYTE, data);
-*/
-//}
-
-//void renderCar(){
-// glPushMatrix();
-/*  glTranslatef(car.pos[0], car.pos[1], car.pos[2]);
-if (!g.silhouette) {
-glBindTexture(GL_TEXTURE_2D, g.bigfootTexture);
-} else {
-
-glBindTexture(GL_TEXTURE_2D, g.silhouetteTexture);
-glEnable(GL_ALPHA_TEST);
-glAlphaFunc(GL_GREATER, 0.0f);
-glColor4ub(255,255,255,255);
-//}
-*/
-
-//      glBegin(GL_QUADS);
-/* if (bigfoot.vel[0] > 0.0) {
-glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
-glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
-glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
-} else {
-*/
-/*                    glTexCoord2f(1.0f, 1.0f); glVertex2i(-wid,-wid);
-		  glTexCoord2f(1.0f, 0.0f); glVertex2i(-wid, wid);
-		  glTexCoord2f(0.0f, 0.0f); glVertex2i( wid, wid);
-		  glTexCoord2f(0.0f, 1.0f); glVertex2i( wid,-wid);
-//}*/
-//  glEnd();
-//  glPopMatrix();
-//
-/*
-if (g.trees && g.silhouette) {
-glBindTexture(GL_TEXTURE_2D, g.forestTransTexture);
-glBegin(GL_QUADS);
-glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 0);
-glTexCoord2f(0.0f, 0.0f); glVertex2i(0, g.yres);
-glTexCoord2f(1.0f, 0.0f); glVertex2i(g.xres, g.yres);
-glTexCoord2f(1.0f, 1.0f); glVertex2i(g.xres, 0);
-glEnd();
-}
-*/
-// glDisable(GL_ALPHA_TEST);
-//}
