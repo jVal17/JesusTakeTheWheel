e01397a88fc0c8ed2574de374a9c025f3ed17e3a
jVal17 <valenzuela171717@gmail.com>
2018-02-24 15:42:35 -0800

    Added header file

---
+++ joseV.h
@@ -0,0 +1,18 @@
+// Jose Valenzuela
+// Header file
+
+#ifndef JOSE_V
+#define JOSE_V
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+using namespace std;
+
+void pauseGame(float y[]); 
+
+#endif
