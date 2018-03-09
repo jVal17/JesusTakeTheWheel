31d6209d2926815cbf02cd81d9a8053b4520f46e
Student <student@lab-315-033.cs.csubak.edu>
2018-03-09 14:57:15 -0800

    updating

--- guillermoR.cpp
+++ guillermoR.cpp
@@ -4,6 +4,25 @@
 #include "guillermoR.h"
 
 //Function is found in the bottom of render() in our main file, "srcfile.cpp".
+
+void totalTimeFunction()
+{
+    static double t = 0.0;
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    int total = 0;
+    Rect r;
+    r.bot = 470;
+    r.left = 215;
+    r.center = 0;
+    for(int i = 0; i < 10000; i++){
+    	total += i/2;
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    t += timeDiff(&start, &end);
+    ggprint8b(&r, 16, 0xFFFF00, "%lf", t);
+}
+
 void drawBox(){
 	glColor3f(0.0, 0.0, 0.0);
 	// Translates the black box beforehand
