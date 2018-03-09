c9504a5f38f9f2ae7aedadfb64ca997751269d1d
Student <student@lab-315-033.cs.csubak.edu>
2018-03-09 14:57:22 -0800

    updating

--- guillermoR.h
+++ guillermoR.h
@@ -8,12 +8,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <cmath>
+#include <ctime>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <GL/glx.h>
 #include "fonts.h"
 using namespace std;
 
+//-----------------------------------------------------------------------------
+//Setup timers
+extern struct timespec timeStart, timeCurrent;
+extern double timeDiff(struct timespec *start, struct timespec *end);
+extern void timeCopy(struct timespec *dest, struct timespec *source);
+//-----------------------------------------------------------------------------
+
+
 void drawBox(); 
+void totalTimeFunction();
 
 #endif
