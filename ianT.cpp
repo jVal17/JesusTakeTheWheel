f8b229577d535a28313e528e62c070bad7dfa763
Ian Thomas <ithomas3@csub.edu>
2018-04-20 23:57:58 -0700

    Fixed style issues.

--- ianT.cpp
+++ ianT.cpp
@@ -1,7 +1,8 @@
 //
-//Name: Ian Thomas
-//My source file
-//Date modified: 03/09/2018
+// Name: Ian Thomas
+// My source file
+// Date modified: 04/20/2018
+// Current goals: Add a fully functioning menu system, add graphics to the menus
 //
 
 #include "ianT.h"
@@ -9,117 +10,118 @@
 
 void renderText()
 {
-    timespec startTime, endTime;
-    static double t = 0.0;
-    clock_gettime(CLOCK_REALTIME, &startTime);
-    Rect r;
-    r.bot = 984;
-    r.left = 20;
-    r.center = 0;
-    ggprint8b(&r, 16, 0x00ff0000, "Jesus Take");
-    ggprint8b(&r, 16, 0x00ff0000, "the Wheel");
-    ggprint8b(&r, 16, 0x00ff0000, "- Controls -");
-    ggprint8b(&r, 16, 0x00ff0000, "p to pause");
-    glEnd();
-    r.bot = 500;
-    r.left = 430;
-    r.center = 0;
-    ggprint8b(&r, 16, 0x00ff0000, "Ian Thomas");
-    glEnd();
-    clock_gettime(CLOCK_REALTIME, &endTime);
-    t += timeDiff(&startTime, &endTime);
-    r.bot = 490;
-    r.left = 420;
-    r.center = 0;
-    ggprint8b(&r, 16, 0x00ff0000, "Timer 1: %.5f", t);
+	timespec startTime, endTime;
+	static double t = 0.0;
+	clock_gettime(CLOCK_REALTIME, &startTime);
+	Rect r;
+	r.bot = 984;
+	r.left = 20;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ff0000, "Jesus Take");
+	ggprint8b(&r, 16, 0x00ff0000, "the Wheel");
+	ggprint8b(&r, 16, 0x00ff0000, "- Controls -");
+	ggprint8b(&r, 16, 0x00ff0000, "esc to pause");
+	ggprint8b(&r, 16, 0x00ff0000, "W,S,A,D to move");
+	glEnd();
+	r.bot = 500;
+	r.left = 430;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ff0000, "Ian Thomas");
+	glEnd();
+	clock_gettime(CLOCK_REALTIME, &endTime);
+	t += timeDiff(&startTime, &endTime);
+	r.bot = 490;
+	r.left = 420;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ff0000, "Timer 1: %.5f", t);
 }
 
 void mainMenu (const int xres, const int yres)
 {
-    unsigned int white = 0xffffff;
+	unsigned int white = 0xffffff;
 
-    Rect m;
-    m.bot = yres - 200;
-    m.left = xres/2 - 55;
-    m.center = 0;
-    ggprint12(&m, 16, white, "Play Game");
-    ggprint12(&m, 16, 0x000000, "Settings **IN PROGRESS**");
+	Rect m;
+	m.bot = yres - 200;
+	m.left = xres/2 - 55;
+	m.center = 0;
+	ggprint12(&m, 16, white, "Play Game");
+	ggprint12(&m, 16, 0x000000, "Settings **IN PROGRESS**");
 
-    int pointerX;
-    int pointerY;
+	int pointerX;
+	int pointerY;
 
-    if (menuPosition == 1) {
-        pointerX = xres/2 - 73;
-        pointerY = yres - 190;
-    } else if (menuPosition == 2) {
-        pointerX = xres/2 - 73;
-        pointerY = yres - 207;
-    }
-
-    float w = 5.0;
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glPushMatrix();
-    glTranslatef(pointerX, pointerY, 0);
-    glColor4ub(255, 255, 255, 255);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i( w, w);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( w,-w);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i(-w,-w);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-w, w);
-    glEnd();
-    glPopMatrix();
-
-   /* if (inMainMenu) {
-	if (key == XK_Return) {
-	    if (menuPosition == 1) {
-                inMainMenu = 0;
-                inGame = 1;
-	    }
+	if (menuPosition == 1) {
+		pointerX = xres/2 - 73;
+		pointerY = yres - 190;
+	} else if (menuPosition == 2) {
+		pointerX = xres/2 - 73;
+		pointerY = yres - 207;
 	}
-    }*/
+
+	float w = 5.0;
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glPushMatrix();
+	glTranslatef(pointerX, pointerY, 0);
+	glColor4ub(255, 255, 255, 255);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i( w, w);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i( w,-w);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i(-w,-w);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-w, w);
+	glEnd();
+	glPopMatrix();
+
+	/*if (inMainMenu) {
+		if (key == XK_Return) {
+			if (menuPosition == 1) {
+				inMainMenu = 0;
+				inGame = 1;
+			}
+		}
+	}*/ // try to implement this after menu is finished.
 }
 
 void pauseMenu (const int xres, const int yres)
 {
-    unsigned int white = 0xffffff;
+	unsigned int white = 0xffffff;
 
-    Rect m;
-    m.bot = yres - 200;
-    m.left = xres/2 -55;
-    m.center = 0;
-    ggprint12(&m, 16, white, "Resume Game");
-    ggprint12(&m, 16, 0x000000, "Settings **IN PROGRESS**");
-    ggprint12(&m, 16, 0x000000, "Controls **IN PROGRESS**");
-    ggprint12(&m, 16, white, "Exit Game");
+	Rect m;
+	m.bot = yres - 200;
+	m.left = xres/2 -55;
+	m.center = 0;
+	ggprint12(&m, 16, white, "Resume Game");
+	ggprint12(&m, 16, 0x000000, "Settings **IN PROGRESS**");
+	ggprint12(&m, 16, 0x000000, "Controls **IN PROGRESS**");
+	ggprint12(&m, 16, white, "Exit Game");
 
-    int pointerX;
-    int pointerY;
+	int pointerX;
+	int pointerY;
 
-    if (menuPosition == 1) {
-	pointerX = xres/2 - 73;
-	pointerY = yres - 190;
-    } else if (menuPosition == 2) {
-	pointerX = xres/2 - 73;
-	pointerY = yres - 207;
-    } else if (menuPosition == 3) {
-	pointerX = xres/2 - 73;
-	pointerY = yres - 224;
-    } else if (menuPosition == 4) {
-	pointerX = xres/2 -73;
-	pointerY = yres - 241;
-    }
+	if (menuPosition == 1) {
+		pointerX = xres/2 - 73;
+		pointerY = yres - 190;
+	} else if (menuPosition == 2) {
+		pointerX = xres/2 - 73;
+		pointerY = yres - 207;
+	} else if (menuPosition == 3) {
+		pointerX = xres/2 - 73;
+		pointerY = yres - 224;
+	} else if (menuPosition == 4) {
+		pointerX = xres/2 -73;
+		pointerY = yres - 241;
+	}
 
-    float w = 5.0;
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glPushMatrix();
-    glTranslatef(pointerX, pointerY, 0);
-    glColor4ub(255, 255, 255, 255);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i( w, w);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( w,-w);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i(-w,-w);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-w, w);
-    glEnd();
-    glPopMatrix();
+	float w = 5.0;
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glPushMatrix();
+	glTranslatef(pointerX, pointerY, 0);
+	glColor4ub(255, 255, 255, 255);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i( w, w);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i( w,-w);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i(-w,-w);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-w, w);
+	glEnd();
+	glPopMatrix();
 }
 
